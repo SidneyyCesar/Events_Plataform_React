b07f63d477998dab88e01af865f698a55f01d8bb
Sidney Cesar <sidneyycesar@hotmail.com>
2022-06-27 09:34:45 -0300

    add app components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { gql, useQuery } from "@apollo/client";
 import { useEffect } from "react"
+import { Header } from "./components/Header";
+import { Lesson } from "./components/Lesson";
+import { Sidebar } from "./components/Sidebar";
+import { Video } from "./components/Video";
 import { client } from "./lib/appolo"
+import { Event } from "./pages/Event";
 
 const GET_LESSONS_QUERY = gql`
 
@@ -19,15 +24,21 @@ interface Lesson {
 
 function App() {
 
-  const  {data} = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY);
-  
   return (
-    <ul>
-      {data?.lessons.map(item => {
-        return <li key={item.id}>{item.title}</li>
-      })}
-    </ul>
+    
+    <Event />
+
   )
+  
+  // const  {data} = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY);
+  
+  // return (
+  //   <ul>
+  //     {data?.lessons.map(item => {
+  //       return <li key={item.id}>{item.title}</li>
+  //     })}
+  //   </ul>
+  // )
 }
 
 export default App
---
+++ src/components/Video.tsx
@@ -0,0 +1,8 @@
+export function Video () {
+  
+  return (
+    <div className="flex-1">
+      <p>Video</p>
+    </div>
+  )
+}
